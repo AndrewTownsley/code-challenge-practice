6221edb39d9660e779032c5733fe00dc6585168f
Andrew TOwnsley <atownsley86@gmail.com>
2021-04-30 23:15:30 -0600

    codewars-practice-file

---
+++ app.js
@@ -0,0 +1,31 @@
+// const list1 = [
+//   { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'C' },
+//   { firstName: 'Anna', lastName: 'R.', country: 'Liechtenstein', continent: 'Europe', age: 52, language: 'JavaScript' },
+//   { firstName: 'Ramon', lastName: 'R.', country: 'Paraguay', continent: 'Americas', age: 29, language: 'Ruby' },
+//   { firstName: 'George', lastName: 'B.', country: 'England', continent: 'Europe', age: 81, language: 'C' },
+//   { firstName: 'c', lastName: 'r.', country: 'England', continent: 'Europe', age: 81, language: 'Python' },
+//   { firstName: 'Bob', lastName: 'B.', country: 'England', continent: 'Europe', age: 57, language: 'Python' },
+// ];
+
+// function getFirstPython(list1) {
+//   //  Find the first Python Developer.
+//     return  firstPython = list1.filter((item) => item.language == 'Python');
+  
+  // Get the firstName and country of that dev.
+  // // console.log(firstPython[0].firstName + ', ' + firstPython[0].country);
+
+  //   }
+
+  //   console.log(getFirstPython(list1));
+   
+
+
+
+//   const findC = list1.filter((item) => item.language == 'C');
+//   const findJavaScript = list1.filter((item) => item.language == 'JavaScript');
+//   const findRuby = list1.filter((item) => item.language == 'Ruby');
+
+// console.table(findC);
+// console.table(findJavaScript);
+// console.table(findRuby);
+
